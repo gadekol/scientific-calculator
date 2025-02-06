ff50a5bb39602c5f5075e5c29e1fa009c6a10596
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-05 21:45:41 -0800

    Update CalculatorResponse.java

--- src/main/java/com/example/calculator/model/CalculatorResponse.java
+++ src/main/java/com/example/calculator/model/CalculatorResponse.java
@@ -1,1 +1,32 @@
+package com.example.calculator.model;
+
+public class CalculationResponse {
+    private double result;
+    private String error;
+
+    public CalculationResponse() {}
+
+    public CalculationResponse(double result) {
+        this.result = result;
+        this.error = null;
+    }
+    
+    public CalculationResponse(double result, String error) {
+        this.result = result;
+        this.error = error;
+    }
+
+    public double getResult() {
+        return result;
+    }
+    public void setResult(double result) {
+        this.result = result;
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
+    }
+}
 
